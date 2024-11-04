4347ccd1c11fac7a7d0d88e5ca52d40c223f2a0d
ValentaCodes <valentacodes@gmail.com>
2024-11-04 11:48:44 -0700

    Update SpeedConverter.java
    removed comments

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -19,15 +19,3 @@ public class SpeedConverter {
         System.out.println(kilosPerHour + " km/h = " + milesPerHour + " mi/h");
     }
 }
-
-//Write another method called printConversion with 1 parameter of type double with the name kilometersPerHour.
-//
-//This method should not return anything (void) and it needs to calculate milesPerHour from the kilometersPerHour parameter.
-//
-//Then it needs to print a message in the format "XX km/h = YY mi/h".
-//
-//XX represents the original value kilometersPerHour.
-//YY represents the rounded milesPerHour from the kilometersPerHour parameter.
-//
-//If the parameter kilometersPerHour is < 0 then print the text "Invalid Value".
-
