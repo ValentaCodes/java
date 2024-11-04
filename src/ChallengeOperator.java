456cdd372938280985ac9385337025cc65afc75c
ValentaCodes <valentacodes@gmail.com>
2024-11-03 23:38:31 -0700

    Not important - SANDBOX

---
+++ src/ChallengeOperator.java
@@ -0,0 +1,14 @@
+public class ChallengeOperator {
+    public static void main(String[] args) {
+        double myDouble = 20.00d;
+        double doubleDouble = 80.00d;
+        double multiDouble = (myDouble + doubleDouble ) * 100.00d;
+        double remainder = multiDouble % 40.00d;
+
+        boolean isNoRemainder = (remainder == 0) ? true : false;
+
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+    }
+}
