abc5c420a4be2fd682b42224626f3054210bf9c7
ValentaCodes <valentacodes@gmail.com>
2024-11-06 08:47:54 -0700

    KiloByte Convertor

---
+++ src/Converter.java
@@ -0,0 +1,17 @@
+public class Converter {
+    public static void main(String[] args) {
+        printMegaByteAndKiloByte(4000);
+    }
+    public static void printMegaByteAndKiloByte(int kilobytes) {
+
+        if (kilobytes < 0) {
+            System.out.println("Invalid Response");
+            return;
+        }
+
+        int megabytes = kilobytes / 1024;
+        int remainder = kilobytes % 1024;
+
+        System.out.print(kilobytes +" KB " + megabytes + " MB and " + remainder + " KB");
+    }
+}
