302079f002131bf8db688982d4f177b36b1a296e
Cornelius Davis <valentacodes@gmail.com>
2025-05-05 22:23:29 -0600

    account class

---
+++ src/bankAccount/Account.java
@@ -0,0 +1,18 @@
+package bankAccount;
+
+public class Account {
+    private double balance;
+    private long accountNum;
+    private String Fname;
+    private String Lname;
+
+
+    public Account(String Fname, String Lname){
+        this.Fname = Fname;
+        this.Lname = Lname;
+    }
+
+    public Account(){
+        
+    }
+}
