533647708323c45fba7aa157fd1e868f57438789
Cornelius Davis <valentacodes@gmail.com>
2025-05-08 16:32:31 -0600

    starting code just playing around

--- src/bankAccount/Main.java
+++ src/bankAccount/Main.java
@@ -2,6 +2,13 @@ package bankAccount;
 
 public class Main {
     public static void main(String[] args) {
-
+        Account ac1 = new Account();
+        ac1.setFname("Cornelius");
+        ac1.setLname("Davis");
+        ac1.setBalance(80000);
+        ac1.setAccountNum(293842398);
+        System.out.println(ac1);
+        ac1.withdrawal(60000);
+        System.out.println(ac1);
     }
 }
