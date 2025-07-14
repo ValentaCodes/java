bfe383627cd7c8885a67ffdf355efc88b916b46d
ValentaCodes <valentacodes@gmail.com>
2025-07-14 10:49:36 -0600

    string methods

--- src/StringBestPractices/Main.java
+++ src/StringBestPractices/Main.java
@@ -2,21 +2,29 @@ package StringBestPractices;
 
 /**
  * @author corneliusdavis
- * @version 07/03/2025
- * The purpose of this package is to understand all about strings in Java
+ * @version 07/03/2025 The purpose of this package is to understand all about strings in Java
  */
 public class Main {
     public static void main(String[] args) {
-    printInformation("Hello world");
-    printInformation("");
-    printInformation("\t   \n");
+        printInformation("Hello world");
+        printInformation("");
+        printInformation("\t   \n");
 
-    String helloWorld = "Hello World";
+        String helloWorld = "Hello World";
+        if (helloWorld.startsWith("Hello")) {
+            System.out.println("This string starts with hello");
+        }
+        if (helloWorld.endsWith("world")) {
+            System.out.println("This string ends with world");
+        }
+        if (helloWorld.contains(" ")) {
+            System.out.println("This string contains ' ' ");
+        }
         System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
         System.out.printf("index of world = %d %n", helloWorld.indexOf("World"));
     }
 
-    public static void printInformation(String string){
+    public static void printInformation(String string) {
         int length = string.length();
         System.out.printf("Length = %d %n", length);
 
